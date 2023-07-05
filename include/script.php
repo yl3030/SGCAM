ce1b599a74ddd5c81b01117b00bb69a524e35061
youzhen <yl301114@gmail.com>
2023-07-05 10:03:23 +0800

    清暫存

--- include/script.php
+++ include/script.php
@@ -1,4 +1,4 @@
 <script src="../js/jquery/jquery.min.js"></script>
 <script src="../js/bootstrap/bootstrap.bundle.min.js"></script>
 <script src="../js/swiper/swiper-bundle.min.js"></script>
-<script src="../js/common.js"></script>
+<script src="../js/common.js?v=20230705"></script>
