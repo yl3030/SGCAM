48dde22ebd6329c298d974459742a8e98d0a4202
youzhen <yl301114@gmail.com>
2023-10-02 00:16:48 +0100

    meta

--- include/meta.php
+++ include/meta.php
@@ -8,4 +8,4 @@
 <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="../css/bootstrap/bootstrap.css">
 <link rel="stylesheet" href="../css/swiper/swiper-bundle.min.css">
-<link rel="stylesheet" href="../css/style.css?v=20230804">
+<link rel="stylesheet" href="../css/style.css?v=20231002">
